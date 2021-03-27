02ece8aab2e2bff84a8acd94a7c8fad1018d3588
gitargy <bdp1312@gmail.com>
2021-03-27 13:39:38 -0500

    ignore .swp

--- Deck.py
+++ Deck.py
@@ -1,8 +1,8 @@
 import Card
 class Deck:
-    suit = ["Club", "Diamond", "Spade", "Heart"]
-    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] 
-    def __init__(self):
+   def __init__(self):
+        suit = ["Club", "Diamond", "Spade", "Heart"]
+        rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] 
         for s in suit:
             for r in rank:
                 card = Card.Card(r, s)
