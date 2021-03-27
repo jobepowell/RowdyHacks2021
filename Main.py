bdf01af592ad9e507ad8569017d88587a4993d11
jobepowell <powell.jobe@gmail.com>
2021-03-27 14:12:56 -0500

    -Generated player hand in main

--- Main.py
+++ Main.py
@@ -4,4 +4,5 @@ import Hand
 #Before Game Setup
 potSize = 0 #Size of the current pot
 deck = Deck.Deck()
-
+playerHand = Hand.Hand(deck.draw(),deck.draw())
+print(playerHand.card1.rank + " of " + playerHand.card1.suit + " and " + playerHand.card2.rank + " of " + playerHand.card2.suit)
