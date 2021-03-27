6a90530c254dadae2c428e234e8683237f293a12
gitargy <bdp1312@gmail.com>
2021-03-27 13:18:10 -0500

    deck init

--- Deck.py
+++ Deck.py
@@ -1,7 +1,12 @@
 import Card
 class Deck:
+    suit = ["Club", "Diamond", "Spade", "Heart"]
+    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] 
     def __init__(self):
-        card = Card.Card('King','Clubs')
-        self.cards = {card : True}
+        for s in suit:
+            for r in rank:
+                card = Card.Card(r, s)
+                self.cards = {card : True}
+ 
 
 
